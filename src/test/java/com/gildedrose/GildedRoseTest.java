27e394c66c84b3dde7cfba320ad70f908f2e387f
JoeBew42 <joebew42@gmail.com>
2015-03-26 11:24:30 +0100

    Initial commit

---
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,18 @@
+package com.gildedrose;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+public class GildedRoseTest {
+
+    @Test
+    public void foo() {
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("fixme", app.items[0].name);
+    }
+
+}
